727c51f686435c1bbbd85f77569dc7c4746dbbe6
Dawin Widjaja <dawin92@yahoo.com>
2023-12-30 13:09:02 +0700

    reconfigure eslint from start using next eslint

--- pages/project-svarga/index.tsx
+++ pages/project-svarga/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import type { NextPage } from 'next';
+import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
 import { Layout } from '../../components/layout/layout';
 
@@ -15,17 +16,17 @@ const SvargaPage: NextPage = () => {
     return (
         <Layout>
             <Container className='text-center'>
-                <img src='svarga.svg' alt='svarga logo' className='mb-4'/>
+                <Image src='svarga.svg' alt='svarga logo' className='mb-4'/>
                 <p>Project: Svarga is a manga that tells about the story of a dying world and the brave heroes that are trying to save it.</p>
                 <p>Synopsis: In a dying world stricken with natural disasters after the first war between humans and gods, 3 nations are facing each other in a war 
                     to conquer Svarga, the last remaining habitable continent rich with natural resources. A princess of one of the warring nations escaped
-                    from her kingdom's grasp and meet with a ragtag band of freedom fighters notoriously known as sky pirates.
+                    from her kingdom&apos;s grasp and meet with a ragtag band of freedom fighters notoriously known as sky pirates.
                     Together, they journey across the world to gain power and bring an end to the war.
                 </p>
 
                 {pageIds.map((id, index) => {
                     return <>
-                        <img className='page-image' src={`https://drive.google.com/uc?export=view&id=${id}`} alt={`page ${index+1}`}/>
+                        <Image className='page-image' src={`https://drive.google.com/uc?export=view&id=${id}`} alt={`page ${index+1}`}/>
                         <p>{index+1}</p>
                     </>;
                 })}
