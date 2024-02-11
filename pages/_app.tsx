7aae7e207d172584cbd12f6dde90081b76dd1977
Dawin Widjaja <dawin92@yahoo.com>
2024-02-11 21:53:19 +0700

    upgrade bootstrap and font awesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { wrapper } from '../redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style.scss';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
