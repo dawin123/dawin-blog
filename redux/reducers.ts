dc700521fb5bdaf0b888ff790015e1684c852799
Dawin Widjaja <dawin92@yahoo.com>
2024-03-03 10:32:41 +0800

    clean up unused redux and related packages

--- redux/reducers.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { blogListReducer, BlogListState } from './blog-list/reducer';
-export interface GlobalState {
-    blogList: BlogListState;
-}
-
-export const reducers = combineReducers({
-    blogList: blogListReducer
-});
